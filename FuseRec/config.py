411c288ab34ad502a161cb9549a9908f7247a469
shaowns <shaown@csebuet.org>
2016-03-08 21:36:12 -0500

    Comments in config.

--- FuseRec/config.py
+++ FuseRec/config.py
@@ -1,7 +1,11 @@
+# The json file containing meta data can be found here:
+# http://static.barik.net/fuse/fuse-bin.analysis.dedup.poi-dec2014.json.gz
+# Extract and set the json_file value to point to the file.
 fuse = dict(
     json_file="./Fuse_Data/fuse-bin"
 )
 
+# File for dumping pickle data.
 rec_data = dict(
     vectors="./Fuse_Data/user_vectors"
 )
