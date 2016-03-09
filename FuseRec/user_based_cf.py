bffe14a7bb3aceb03991ffe3a252dc503ea0960b
shaowns <shaown@csebuet.org>
2016-03-08 22:27:23 -0500

    Added cosine distance function (faster).

--- FuseRec/user_based_cf.py
+++ FuseRec/user_based_cf.py
@@ -5,6 +5,37 @@ from pickle import load as pl
 import config
 
 
+# Get cosine distance between two vectors x and y.
+def get_cosine_dist(x, y):
+    # Swap positions so that we have to do less calculations.
+    if len(y) < len(x):
+        x, y = y, x
+
+    res = 0
+    for key, x_val in x.iteritems():
+        res += x_val * y.get(key, 0)
+    if res == 0:
+        return 1
+
+    # Get normalized vector values.
+    norm_x = sum([v**2 for v in x.itervalues()])
+    norm_y = sum([v**2 for v in y.itervalues()])
+
+    try:
+        res = res / (norm_x * norm_y)
+    except ZeroDivisionError:
+        # In case we were bad somewhere.
+        res = 1
+
+    return 1 - res
+
+
+# For a given user u, get back a dictionary of cosine distance of each vector in set v.
+def get_cosine_distance_for_user(u, v):
+    # TODO
+    return
+
+
 # Natural log of total users / no. of users using function f in vector set v.
 def get_inverse_user_freq(f, v):
     return log(len(v)/sum([f in data.keys() for data in v.itervalues()]))
