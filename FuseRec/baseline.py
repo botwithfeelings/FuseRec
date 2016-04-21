41a46272dc39f5118468ec13a5f1f4d325b3cca3
Nisarg Vinchhi <nvinchh@ncsu.edu>
2016-04-21 18:09:24 -0400

    Refined it. Need more inputs

--- FuseRec/baseline.py
+++ FuseRec/baseline.py
@@ -3,51 +3,8 @@ import config
 from itertools import islice
 import operator
 
+'''
 dataset = utility.load_vectors()
-
-def take(n, iterable):
-    "Return first n items of the iterable as a list"
-    return list(islice(iterable, n))
-
-def baseline():
-    for i in range(config.num_slices):
-        train, test = utility.get_data_split(dataset, i)
-        # Retrieving functions and the counts from each training set
-        orderedFunctions = getOrderedFunctions(train)
-        totalRecommendations = recommendBaseline(orderedFunctions, test)
-
-
-def recommendBaseline(orderedFunctions, testset):
-    top10 = orderedFunctions[:10]
-    totalRecommendations = []
-    for (user, data) in testset.iteritems():
-        foreachuser = []
-        for function in top10:
-            if data.has_key(function)==False:
-                foreachuser.append(function)
-        totalRecommendations.append(foreachuser)
-
-    return totalRecommendations
-
-def getOrderedFunctions(dataset):
-    functionCount = dict()
-    for (user, data) in dataset.iteritems():
-        for (function, count) in data.iteritems():
-            if functionCount.has_key(function):
-                fcount = functionCount.get(function)
-                fcount = fcount + count
-                functionCount[function] = fcount
-            else:
-                functionCount[function] = count
-
-    orderedFunctionList = sorted(functionCount, key=functionCount.__getitem__, reverse=True)
-
-    return orderedFunctionList
-
-
-
-
-'''dataset = utility.load_vectors()
 function_total = dict()
 print len(function_total)
 for (user, data) in dataset.iteritems():
@@ -58,25 +15,74 @@ for (user, data) in dataset.iteritems():
             function_total[function] = fcount
         else:
             function_total[function] = count
+mostFrequent = sorted(function_total, key=function_total.get, reverse=True)
+'''
+def getMostFrequent(dataset):
+    function_total = dict()
+    for (user, data) in dataset.iteritems():
+        for (function, count) in data.iteritems():
+            if function_total.has_key(function):
+                fcount = function_total.get(function)
+                fcount = fcount + count
+                function_total[function] = fcount
+            else:
+                function_total[function] = count
+    mostFrequent = sorted(function_total, key = function_total.get, reverse=True)
+    return mostFrequent
+
+def baseline(dataset):
+    successPerSet = []
+    for i in range(config.num_slices):
+        train, test = utility.get_data_split(dataset, i)
+        # Retrieving functions and the counts from each training set
+        mFrequentFunctions = getMostFrequent(train)
+        success = recommendBaseline(mFrequentFunctions, test)
+        successPerSet.append(success)
+        print successPerSet
 
 
+def recommendBaseline(orderedFunctions, testset):
+    recList = []
+    success = 0
+    for (user, data) in testset.iteritems():
+        fList = data.keys()
+        rmFunction = data.popitem()
+        #print type(rmFunction)
+        for func in orderedFunctions:
+            if func not in fList:
+                recList.append(func)
+            if len(recList) == 10:
+                break
+        if rmFunction[0] in recList:
+            success = success + 1
+    return success
+
+
+def main():
+    dataset = utility.load_vectors()
+    baseline(dataset)
+
+if __name__== "__main__":
+    main()
 
 #nitems = take(4, sorted(function_total.iteritems(), key=operator.itemgetter(1), reverse=True))
 #print nitems
 '''
-dict2 = {'x':5, 'a': 7, 'b':4, 'z':12}
-sDict = sorted(dict2, key=dict2.__getitem__, reverse=True)
+dict2 = {'x':5, 'a': 7, 'b':4, 'z':12, 'm':30}
+sDict = dict2.keys()
 #sDict = sorted(dict2.values(), reverse=True)
 #s2 = sorted(dict2)
 #print type(s2)
 print sDict
-#print type(sDict[0])
+item = dict2.popitem()
+print item
+print type(sDict)
 #print sDict.has_key('z')
 #nitems2 = take(3, sDict)
 #print nitems2['z']
 #print nitems2
 #print len(function_total)
-'''
+
 listx = dataset.values()
 firstdict = listx[1]
 print firstdict.get('Divide')
