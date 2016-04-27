Added the fix for the inverse frequency calculation, closes #12

FuseRec/user_based_cf.py
@@ -14,9 +14,9 @@ def get_cosine_similarity_for_user(vw, user):
 
 
 # Get a list of recommendations for a given user ID.
-def get_recommendations(data, user):
+def get_recommendations(data, user, cache):
     # Get the weighted vector for current data.
-    vw = utility.get_weighted_vectors(data)
+    vw = utility.get_weighted_vectors(data, cache)
 
     # Get the sorted similarity values for the given user.
     sims = get_cosine_similarity_for_user(vw, user)
@@ -43,25 +43,48 @@ def get_recommendations(data, user):
     return recs
 
 
+def generate_usage_cache(data):
+    cache = dict()
+    for d in data.itervalues():
+        cache.update((func, cache.get(func, 0) + 1) for (func, freq) in d.iteritems())
+    return cache
+
+
+def add_to_cache(cache, data):
+    cache.update((func, cache.get(func, 0) + 1) for (func, freq) in data.iteritems())
+    return cache
+
+
+def remove_from_cache(cache, data):
+    cache.update((func, cache.get(func, 0) - 1) for (func, freq) in data.iteritems())
+    return cache
+
+
 def do_user_cf(train, test):
+    # Cache that contains how many user uses a certain function.
+    usage_cache = generate_usage_cache(train)
     success = 0
-    count = 0
+
     for (user, data) in test.iteritems():
-        print "test user " + str(count) + " of " + str(len(test))
-        count += 1
         # The function to be removed at random.
         test_func = np.random.choice(data.keys())
         data.pop(test_func)
+
+        # Update the cache with the test case.
+        usage_cache = add_to_cache(usage_cache, data)
 
         # Add this modified vector to the training set.
         train[user] = data
 
         # Get the recommendation for the user in training data.
-        if test_func in get_recommendations(train, user):
+        if test_func in get_recommendations(train, user, usage_cache):
             success += 1
-            print "success"
+
         # Removed the test user from the training data for next iteration.
         train.pop(user)
+
+        # Update the cache again.
+        usage_cache = remove_from_cache(usage_cache, data)
 
     return success
 
@@ -72,10 +95,9 @@ def do_cv():
     outfile_string = "user_slice" + str(config.num_slices) + "_rec" \
         + str(config.tuning_param['num_recs']) + "_users" + str(config.tuning_param['num_sims']) + ".txt"
     rates = list()
-    st = state('User Based', rates, outfile_string, "INFO", \
-        config.num_slices, config.tuning_param['num_recs'], config.tuning_param['num_sims'])
+    st = state('User Based', rates, outfile_string, "INFO",
+               config.num_slices, config.tuning_param['num_recs'], config.tuning_param['num_sims'])
     # Storage for the success rate.
-    rates = list()
     for i in range(st.num_slices):
         print "current slice: ", st.cur_slice
         st.cur_slice += 1